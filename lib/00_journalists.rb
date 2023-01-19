4371d41a7a8145ced18d85ed3553cbf0250e0eec
blobby-bobby <bulle.ouvrard@gmail.com>
2023-01-19 16:40:26 +0100

    it's the ruby files

---
+++ lib/00_journalists.rb
@@ -0,0 +1,95 @@
+# La liste des handles de journalistes à traiter
+journalistes =  ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#Combien y a-t-il de handle dans cette array ?
+def handles_length(handles)
+    return handles.length
+end
+
+#Quel est le handle le plus court de cette liste ?
+def shortest_handle(handles)
+    return handles.min_by {|x| x.length }
+end
+
+#Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+def five_str_handle(handles)
+    return handles.count {|x| x.length == 6 }
+end
+
+#Combien commencent par une majuscule (première lettre juste après le @) ?
+def uppercase_handle(handles)
+    return handles.count {|x| x[1] =~ /[A-Z]/ }
+end
+
+#Trie la liste de handle par ordre alphabétique.
+def alphabetical_order(handles)
+    return handles.sort do |personne_a, personne_b| 
+        personne_a <=> personne_b
+    end.first(10).join(", ")
+end
+
+#Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+def handle_size_min_to_max(handles)
+    return handles.sort_by {|x| x.length}.first(20).join(", ")
+end
+
+#Quelle est la position dans l'array de la personne @epenser ?
+def e_penser(handles)
+    return handles.index("@epenser")
+end
+
+#Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+def tri_caracteres(handles)
+    for number in 1..(handles.max {|a, b| a.length <=> b.length}).length
+            puts "Nombre de handles avec #{number} caractères : #{handles.count {|i| i.length == number}}"
+    end
+end
+
+
+def perform(handles)
+
+    puts "---"
+    puts "Bienvenue dans Twittos News, le Big Data des journalistes français !!! Plein d'infos dont tu te fiches maintenant !"
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "---"
+    puts "Dans la Twittosphère française il y a " + handles_length(handles).to_s + " journalistes"
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "---"
+    puts "Le handle le plus court est : " + shortest_handle(handles).to_s
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "---"
+    puts "Nombre de twittos journaleux dont le handle a 5 caractères : " + five_str_handle(handles).to_s
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "---"
+    puts "Nombre de twittos qui ont mis une majuscule sur leur handle : " + uppercase_handle(handles).to_s
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "---"
+    puts "Mettons les 10 premiers qui apparaissent quand on les range par ordre alphabétique : " 
+    puts alphabetical_order(handles).to_s
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "---"
+    puts "Voici les 20 handles les plus courts chez les journaleux : " 
+    puts handle_size_min_to_max(handles).to_s
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+
+    puts "e-penser est numéro : " + e_penser(handles).to_s
+    puts "---"
+    puts "Tape 'Entrée' pour avoir une info pas utile en plus"
+    gets.chomp
+tri_caracteres(handles)
+end
+
+perform(journalistes)
